095530d823c96f8af8085885908bc5fb97e331ad
Don Alvarez <alvarezdon@outlook.com>
2019-05-23 18:55:01 -0700

    Add storybook addons, bump create-react-app to 3.0.1

--- web-app/.storybook/webpack.config.js
+++ web-app/.storybook/webpack.config.js
@@ -6,16 +6,33 @@ module.exports = ({config}) => {
     config.module.rules.push({
         test: /\.(ts|tsx)$/,
         include: [SRC_PATH, STORIES_PATH],
-            use: [
-                {
-                    loader: require.resolve('awesome-typescript-loader'),
-                    options: {
-                        configFileName: './.storybook/tsconfig.json'
-                    }
-                },
-                { loader: require.resolve('react-docgen-typescript-loader') }
-            ]
+        use: [
+            {
+                loader: require.resolve('awesome-typescript-loader'),
+                options: {
+                    configFileName: './.storybook/tsconfig.json'
+                }
+            },
+            { loader: require.resolve('react-docgen-typescript-loader') }
+        ]
     });
+    config.module.rules.push({
+        test: /\.stories\.tsx?$/,
+        include: [SRC_PATH, STORIES_PATH],
+        use: [
+            {
+                loader: require.resolve('@storybook/addon-storysource/loader'),
+                options: { 
+                    parser: 'typescript',
+                    prettierConfig: {
+                        tabWidth: 4,
+                        singleQuote: false,
+                    },
+                },
+            },
+        ],
+        enforce: 'pre',
+      });
     config.resolve.extensions.push('.ts', '.tsx');
     return config;
 };
